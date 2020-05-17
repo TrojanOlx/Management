9ab6d5a834809c7d9203c65d2e92db84c90307a9
龙兴 欧 <815195981@qq.com>
2020-05-18 01:08:13 +0800

    menu ListView

--- Management/Management/MainPage.xaml.cs
+++ Management/Management/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Management.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace Management
         public MainPage()
         {
             InitializeComponent();
+            this.BindingContext = new MainViewModel();
         }
     }
 }
