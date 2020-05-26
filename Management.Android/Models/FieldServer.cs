186bb70d7ee93911247f673323ad2830494e7c04
龙兴 欧 <815195981@qq.com>
2020-05-26 22:40:34 +0800

    Model

---
+++ Management.Android/Models/FieldServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Management.Android.Models
+{
+    public class FieldServer
+    {
+        public class Field
+        {
+            public Field()
+            {
+
+            }
+            public Field(int id, string name, string key)
+            {
+                Id = id;
+                Name = name;
+                Key = key;
+            }
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Key { get; set; }
+
+        }
+
+        public List<Field> Fields { get; set; }
+
+        public List<Field> GetFields()
+        {
+            List<Field> fields = new List<Field>();
+
+            fields.Add(new Field(1,"编号","id"));
+            fields.Add(new Field(2, "名称", "name"));
+            fields.Add(new Field(1, "备注", "remark"));
+
+            Fields = fields;
+            return fields;
+        }
+
+
+
+    }
+
+
+
+
+
+}
---
+++ Management.Android/Models/MaterialServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Management.Android.Models
+{
+    class MaterialServer
+    {
+        public Dictionary<string, object> Material { get; set; } = new Dictionary<string, object> {
+            { "id", 1 },
+            { "name", "材料名称" },
+            { "remark", "备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注" }
+        };
+    }
+}
