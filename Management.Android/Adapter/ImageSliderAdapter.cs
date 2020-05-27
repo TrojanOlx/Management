8af9e812e7e34ee22ac7e7e886e3200ce9b805dd
Trojan <email@oulongxing.com>
2020-05-27 13:49:45 +0800

    加入图片滚动

---
+++ Management.Android/Adapter/ImageSliderAdapter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.View;
+using Android.Views;
+using Android.Widget;
+using Com.Bumptech.Glide;
+using Java.Lang;
+using Java.Net;
+using Management.Android.Fragments;
+using Object = Java.Lang.Object;
+
+namespace Management.Android.Adapter
+{
+    /// <summary>
+    /// 轮播图适配器
+    /// </summary>
+    public class ImageSliderAdapter : PagerAdapter
+    {
+
+        private readonly Context _context;
+        private readonly List<string> _imageList;
+
+        public ImageSliderAdapter(Context context, List<string> imageList)
+        {
+            _context = context;
+            _imageList = imageList;
+        }
+
+        public override bool IsViewFromObject(View view, Object @object)
+        {
+            return view == (LinearLayout)@object;
+        }
+
+        public override Object InstantiateItem(ViewGroup container, int position)
+        {
+
+            position = position % Count;
+
+            View view = container;
+            var inflater = _context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
+            view = inflater.Inflate(Resource.Layout.image_slider_item, null);
+            var child = view.FindViewById<ImageView>(Resource.Id.image_slider_item);
+            // 点击事件
+            child.Click += (sender, args) =>
+            {
+                Toast.MakeText(_context, $"{_imageList[position]}", ToastLength.Short).Show();
+            };
+
+
+            Glide.With(_context).AsBitmap().Load(_imageList[position]).Into(child);
+
+
+            //Bitmap image = null;
+            //Task.Run(() =>
+            //{
+            //    URL url = new URL(_imageList[position]);
+            //    image = BitmapFactory.DecodeStream(url.OpenConnection().InputStream);
+            //}).ContinueWith(t =>
+            //{
+            //    (_context as FruitActivity)?.RunOnUiThread(() =>
+            //    {
+            //        child.SetImageBitmap(image);
+            //    });
+            //});
+
+            container.AddView(view);
+            return view;
+        }
+
+        //public override ICharSequence GetPageTitleFormatted(int position)
+        //{
+        //    return new Java.Lang.String("Problem " + (position + 1));
+        //}
+
+        public override void DestroyItem(ViewGroup container, int position, Java.Lang.Object @object)
+        {
+            container.RemoveView((View)@object);
+        }
+
+        /// <summary>
+        /// 返回总数量
+        /// </summary>
+        public override int Count => _imageList.Count;
+    }
+}
--- Management.Android/Fragments/FruitActivity.cs
+++ Management.Android/Fragments/FruitActivity.cs
@@ -1,12 +1,15 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+using Android.App;
 using Android.OS;
 using Android.Support.Design.Widget;
+using Android.Support.V4.View;
 using Android.Support.V7.App;
 using Android.Widget;
 using Toolbar = Android.Support.V7.Widget.Toolbar;
 using Com.Bumptech.Glide;
 using Android.Views;
 using Java.Lang;
+using Management.Android.Adapter;
 using AndroidResource = Android.Resource;
 
 namespace Management.Android.Fragments
@@ -29,19 +32,34 @@ namespace Management.Android.Fragments
 
             Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.tool_bar);
             CollapsingToolbarLayout collapsingToolbar = FindViewById<CollapsingToolbarLayout>(Resource.Id.collapsing_toolbar);
-            ImageView fruitImageView = FindViewById<ImageView>(Resource.Id.fruit_image_view);
+            //ImageView fruitImageView = FindViewById<ImageView>(Resource.Id.fruit_image_view);
             TextView fruitContentText = FindViewById<TextView>(Resource.Id.fruit_content_text);
-            
+
+            //toolbar.Title = fruitName;
             SetSupportActionBar(toolbar);
+
 
             // 添加返回键
             SupportActionBar.SetDisplayHomeAsUpEnabled(true);
-            
+
+            //collapsingToolbar.SetCollapsedTitleTextAppearance(Resource.Style.CollapsedAppBar);
+            //collapsingToolbar.SetExpandedTitleTextAppearance(Resource.Style.CollapsedAppBar);
             collapsingToolbar.SetTitle(fruitName);
 
-            Glide.With(this).Load(fruitImageId).Into(fruitImageView);
+            //Glide.With(this).Load(fruitImageId).Into(fruitImageView);
             //Glide.With(this).AsBitmap().Load("").Into(fruitImageView);
             fruitContentText.Text = Add500(fruitName);
+
+
+            var imageViewer = FindViewById<ViewPager>(Resource.Id.image_pager);
+            imageViewer.Adapter = new ImageSliderAdapter(this, new List<string>()
+            {
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590552625990&di=8088736bfaac32f8808861d97edf6d70&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fwallpaper%2F1208%2F15%2Fc0%2F12924355_1344999165562.jpg",
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590552625990&di=2af594f5495d6f2fc3eb19e023ae0914&imgtype=0&src=http%3A%2F%2Fpic.baike.soso.com%2Fp%2F20131221%2F20131221152446-229545202.jpg",
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590552625989&di=07248ec9129a47b044881e5df9524d1f&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201308%2F24%2F162824zuojehbb2u4kbula.jpg"
+            });
+
+
         }
 
         public override bool OnOptionsItemSelected(IMenuItem item)
